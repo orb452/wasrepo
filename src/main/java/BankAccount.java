ce4cf365a23cad103d7f38fd065455faafe51fc2
orb452 <sarayutechit@gmail.com>
2017-01-06 07:50:57 -0500

    Update BankAccount.java

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -2,19 +2,19 @@ package com.bogotobogo;
 
 public class BankAccount {
 
-	  private double balance;
+          private double balance;
 
-	  public BankAccount(double balance) {
-	    this.balance = balance;
-	  }
+          public BankAccount(double balance) {
+            this.balance = balance;
+          }
 
-	  public double debit(double amount) {
-	    if (balance < amount) {
-	      amount = balance;
-	    }
+          public double debit(double amount) {
+            if (balance < amount) {
+              amount = balance;
+            }
 
-	    balance -= amount;
-	    return amount;
-	  }
+            balance -= amount;
+            return amount;
+          }
 
 }
